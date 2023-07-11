90caac421be019b1eb16f9d33da49d50c52bcd2c
Pratik Joseph Dabre <80276547+pdabre12@users.noreply.github.com>
2023-07-11 16:26:27 -0700

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 const config = {
-    bucketName: 'hunger-monkey',
-    dirName: 'restaurants', /* optional */
-    region: 'us-west-2',
-    accessKeyId: 'AKIAULGZ2IWK75RU6BLW',
-    secretAccessKey: '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'
+    bucketName: 'xxxxx',
+    dirName: 'xxxx', /* optional */
+    region: 'xxxx',
+    accessKeyId: 'xxxx',
+    secretAccessKey: 'xxxxx'
 }
 
 export default config;
